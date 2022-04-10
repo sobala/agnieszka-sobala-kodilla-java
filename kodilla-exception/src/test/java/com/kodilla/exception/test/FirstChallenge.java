ac0cb6aacfd1e1b0fe4bed5bb0ceedeac483acee
Agnieszka <agnieszka.sobala@yandex.com>
2022-04-11 01:30:28 +0200

    8.2 Added try-catch-finally bloc

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("You tried to divide by 0");
+        } finally {
+            System.out.println("Couldn't calculate the result");
+        }
+
+    }
+}
