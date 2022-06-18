e30351a616f3872a9524ffe5ba0e203d2445b933
Agnieszka <agnieszka.sobala@yandex.com>
2022-06-18 15:45:31 +0100

    Learning spring beans

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootTest
 public class LibraryTestSuite {
 
@@ -15,11 +17,8 @@ public class LibraryTestSuite {
     @Test
     void testLoadFromDb() {
         //Given
-
-
         //When
         library.loadFromDb();
-
         //Then
         //do nothing
     }
@@ -27,12 +26,22 @@ public class LibraryTestSuite {
     @Test
     void testSaveToDb() {
         //Given
-
-
         //When
         library.saveToDb();
-
         //Then
         //do nothing
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & Then
+        System.out.println("===== Beans list: ==== >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< ===== Beans list ====");
     }
 }
